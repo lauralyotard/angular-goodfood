b9dcf457a472b701efd8c491adc8a902a3988aeb
Laura <laura.direct@yahoo.com>
2022-06-02 23:16:50 +0200

    add architecture

---
+++ src/app/pizzaList/pizzaList.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizzaList',
+  templateUrl: './pizzaList.component.html',
+  styleUrls: ['./pizzaList.component.css']
+})
+export class PizzalistComponent {
+  title = 'angular-goodfood';
+}
