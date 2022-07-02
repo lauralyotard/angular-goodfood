5b61a978b2639599890e931cf508574e46a7ce1e
Laura <laura.direct@yahoo.com>
2022-07-02 16:44:45 +0200

    popup ajoutee

--- src/app/compte/account.component.ts
+++ src/app/compte/account.component.ts
@@ -22,31 +22,26 @@ export class AccountComponent {
       case 1: {
         numero = 1
         this.num = numero
-        console.log(this.num + 'here')
         break;
       }
       case 2: {
         numero = 2
         this.num = numero
-        console.log(this.num + 'heree')
         break;
       }
       case 3: {
         numero = 3
         this.num = numero
-        console.log(this.num + 'hereee')
         break;
       }
       case 4: {
         numero = 4
         this.num = numero
-        console.log(this.num + 'hereeee')
         break;
       }
       default: {
         numero = 1
         this.num = numero
-        console.log(this.num + 'heeeere')
         break;
       }
     }
